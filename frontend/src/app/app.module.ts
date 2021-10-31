ec707654ff6e91e7367001ef38c83246e6a38329
wslmacieira <wslmacieira@outlook.com>
2021-10-31 10:19:10 -0300

    Cria os components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeedComponent } from './feed/feed.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    FeedComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
